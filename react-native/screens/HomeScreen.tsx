7cb79bded3311395ec047991efac66d96c8e9811
Francin Vincent <francin.vinc@gmail.com>
2022-09-20 22:47:46 +0200

    rewritten navigation with game screen

--- react-native/screens/HomeScreen.tsx
+++ react-native/screens/HomeScreen.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Wrapper } from "../components/layout/Wrapper";
-
-import { Text } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
+import { Pressable, Text } from "react-native";
 
 export const HomeScreen = ({ navigation }: RootTabScreenProps<"Home">) => {
 
   return (
     <Wrapper>
-      <Text>Home Screen</Text>
+      <Pressable onPress={() => navigation.navigate("Game", { gameType: 'W' })}>
+        <Text>Wordle</Text>
+      </Pressable>
+      <Pressable onPress={() => navigation.navigate("Game", { gameType: 'B' })}>
+        <Text>Behind the box</Text>
+      </Pressable>
+      <Pressable onPress={() => navigation.navigate("Game", { gameType: 'G' })}>
+        <Text>Hangman</Text>
+      </Pressable>
     </Wrapper>
   );
 };
